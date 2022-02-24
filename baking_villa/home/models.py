d7cc554343a2930fe2bf4b6ab977421ef9e6e97a
sarathvikram <saviksavik62@gmail.com>
2022-02-24 15:32:55 +0530

    cake model was created but not make any migrations

--- baking_villa/home/models.py
+++ baking_villa/home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class cake():
-    name : str
-    image : str
-    price : int
+class cake(models.Models):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pics')
+    price = models.IntegerField()
+    stock = models.FloatField()
+    
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    offer = models.FloatField()
+    offer_offer2 = models.BooleanField(default=False)
+    
+
+
+
